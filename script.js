5bfdc41c9fe853b1a2b42d9f85397a96d452107a
J.A.S <jas_archi@outlook.com>
2026-07-07 13:10:28 -0700

    testing a 3d cube insert

---
+++ script.js
@@ -0,0 +1,43 @@
+// 3D MODEL FOR SCENE 3 of LANDING PAGE
+
+import * as THREE from 'three';
+
+const container = document.getElementById('model-container');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    container.clientWidth / container.clientHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(
+    container.clientWidth,
+    container.clientHeight
+);
+
+container.appendChild(renderer.domElement);
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshNormalMaterial()
+);
+
+scene.add(cube);
+
+camera.position.z = 3;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();

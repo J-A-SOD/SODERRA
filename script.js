e225cc4c588b4da791970d28089fb2d1a6d7879f
J.A.S <jas_archi@outlook.com>
2026-07-07 19:22:43 -0700

    adding model

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ console.log("script loaded");
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { OutlinePass } from 'three/addons/postprocessing/OutlinePass.js';
+
 
 
 const container = document.getElementById('model-container');
@@ -13,14 +17,26 @@ console.log(container);
 
 const scene = new THREE.Scene();
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
 const camera = new THREE.PerspectiveCamera(
-    75,
+    25,
     container.clientWidth / container.clientHeight,
     0.1,
     1000
 );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+});
+
 
 renderer.setSize(
     container.clientWidth,
@@ -29,49 +45,234 @@ renderer.setSize(
 
 container.appendChild(renderer.domElement);
 
+const composer = new EffectComposer(renderer);
 
-import { GLTFLoader } from './libs/GLTFLoader.js';
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
 
-const loader = new GLTFLoader();
-
-loader.load(
-    './assets/models/cylinder.glb',
-    function (gltf) {
-        const model = gltf.scene;
-
-        model.scale.set(1, 1, 1);
-        model.position.set(0, 0, 0);
-
-        scene.add(model);
-
-        animate();
-
-        function animate() {
-            requestAnimationFrame(animate);
-
-            model.rotation.y += 0.01;
-
-            renderer.render(scene, camera);
-        }
-    },
-    undefined,
-    function (error) {
-        console.error(error);
-    }
+const outlinePass = new OutlinePass(
+    new THREE.Vector2(
+        container.clientWidth,
+        container.clientHeight
+    ),
+    scene,
+    camera
 );
 
+camera.position.z = 103;
+const models = [];
+const loader = new GLTFLoader();
 
-camera.position.z = 3;
+function convertToOutline(model) {
+
+    model.traverse((child) => {
+
+        if (child.isMesh) {
+
+            const edges = new THREE.EdgesGeometry(
+                child.geometry,
+                5
+            );
+
+                        
+            const outline = new THREE.LineSegments(
+                edges,
+                new THREE.LineBasicMaterial({
+                    color: '#fcfcfc',
+                })
+            );
+
+            outline.renderOrder = 1;
+
+
+            outline.position.copy(child.position);
+            outline.rotation.copy(child.rotation);
+            outline.scale.copy(child.scale);
+            
+            child.material = new THREE.MeshBasicMaterial({
+                color: '#4a484e',
+            });
+
+            child.parent.add(outline);
+
+            child.visible = true;
+        }
+
+    });
+
+}
+
+
+let staticbase;
+let piano;
+let speakerlarge;
+let speakersmall;
+let camerapos;
+let cameraneg;
+let cameralens;
+let arch;
+let helix;
+let cogsneg;
+let cogspos;
+
+// loading the models //
+loader.load('./assets/models/staticbase.glb', (gltf) => {
+    staticbase = gltf.scene;
+    convertToOutline(staticbase);
+    staticbase.position.y = -24;
+    staticbase.scale.set(2, 2, 2);
+    scene.add(staticbase);
+});
+
+loader.load('./assets/models/piano.glb', (gltf) => {
+    piano = gltf.scene;
+    convertToOutline(piano);
+    piano.scale.set(2, 2, 2);
+    piano.position.y = -24;
+    scene.add(piano);
+});
+
+loader.load('./assets/models/speakerlarge.glb', (gltf) => {
+    speakerlarge = gltf.scene;
+    convertToOutline(speakerlarge);
+    speakerlarge.scale.set(2, 2, 2);
+    speakerlarge.position.y = -24;
+    scene.add(speakerlarge);
+});
+
+loader.load('./assets/models/speakersmall.glb', (gltf) => {
+    speakersmall = gltf.scene;
+    convertToOutline(speakersmall);
+    speakersmall.scale.set(2, 2, 2);
+    speakersmall.position.y = -24;
+    scene.add(speakersmall);
+});
+
+loader.load('./assets/models/camerapos.glb', (gltf) => {
+    camerapos = gltf.scene;
+    convertToOutline(camerapos);
+    camerapos.scale.set(2, 2, 2);
+    camerapos.position.y = -24;
+    scene.add(camerapos);
+});
+
+loader.load('./assets/models/cameraneg.glb', (gltf) => {
+    cameraneg = gltf.scene;
+    convertToOutline(cameraneg);
+    cameraneg.scale.set(2, 2, 2);
+    cameraneg.position.y = -24;
+    scene.add(cameraneg);
+});
+
+loader.load('./assets/models/cameralens.glb', (gltf) => {
+    cameralens = gltf.scene;
+    convertToOutline(cameralens);
+    cameralens.scale.set(2, 2, 2);
+    cameralens.position.y = -24;
+    scene.add(cameralens);
+});
+
+loader.load('./assets/models/arch.glb', (gltf) => {
+    arch = gltf.scene;
+    convertToOutline(arch);
+    arch.scale.set(2, 2, 2);
+    arch.position.y = -24;
+    scene.add(arch);
+});
+
+loader.load('./assets/models/helix.glb', (gltf) => {
+    helix = gltf.scene;
+    convertToOutline(helix);
+    helix.scale.set(2, 2, 2);
+    helix.position.y = -24;
+    scene.add(helix);
+});
+
+loader.load('./assets/models/cogsneg.glb', (gltf) => {
+    cogsneg = gltf.scene;
+    convertToOutline(cogsneg);
+    cogsneg.scale.set(2, 2, 2);
+    cogsneg.position.y = -24;
+    scene.add(cogsneg);
+});
+
+loader.load('./assets/models/cogspos.glb', (gltf) => {
+    cogspos = gltf.scene;
+    convertToOutline(cogspos);
+    cogspos.scale.set(2, 2, 2);
+    cogspos.position.y = -24;
+    scene.add(cogspos);
+});
+
+let pianoRotation = 0;
+let speakerlargeRotation = 0;
+let speakersmallRotation = 0;
+let cameraposRotation = 0;
+let cameranegRotation = 0;
+let cameralensRotation = 0;
+let archRotation = 0;
+let helixRotation = 0;
+let cogsnegRotation = 0;
+let cogsposRotation = 0;
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const scroll = window.scrollY;
 
-    renderer.render(scene, camera);
+    if (piano) {
+        const targetRotation = scroll * 0.002;
+        pianoRotation += (targetRotation - pianoRotation) * 0.05;
+        piano.rotation.y = pianoRotation;
+    }
+    
+    if (speakerlarge) {
+        speakerlargeRotation += (scroll * -0.002 - speakerlargeRotation) * 0.05;
+        speakerlarge.rotation.y = speakerlargeRotation;
+    }
+
+    if (speakersmall) {
+        speakersmallRotation += (scroll * 0.002 - speakersmallRotation) * 0.05;
+        speakersmall.rotation.y = speakersmallRotation;
+    }   
+
+    if (camerapos) {
+        cameraposRotation += (scroll * 0.002 - cameraposRotation) * 0.05;
+        camerapos.rotation.y = cameraposRotation;
+    }   
+
+    if (cameraneg) {           
+        cameranegRotation += (scroll * -0.002 - cameranegRotation) * 0.05;
+        cameraneg.rotation.y = cameranegRotation;
+    }   
+
+    if (cameralens) {
+        cameralensRotation += (scroll * 0.002 - cameralensRotation) * 0.05;
+        cameralens.rotation.y = cameralensRotation;
+    }   
+    
+    if (arch) {    
+        archRotation += (scroll * 0.002 - archRotation) * 0.05;
+        arch.rotation.y = archRotation;
+    }
+
+    if (helix) {
+        helixRotation += (scroll * 0.001 - helixRotation) * 0.05;
+        helix.rotation.y = helixRotation;
+    }   
+
+    if (cogsneg) {
+        cogsnegRotation += (scroll * 0.002 - cogsnegRotation) * 0.05;
+        cogsneg.rotation.y = cogsnegRotation;
+    }   
+
+    if (cogspos) {
+        cogsposRotation += (scroll * -0.008 - cogsposRotation) * 0.05;
+        cogspos.rotation.y = cogsposRotation;
+    }
+
+    camera.lookAt(0, camera.position.y, 0);
+    composer.render();
 }
-
-renderer.setClearColor(0xff0000);
 
 animate();

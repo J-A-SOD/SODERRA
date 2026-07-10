6ec3abd9b08e450c7b5de3ea7f2ea3fe39f0416e
J.A.Soderstrom <jas_archi@outlook.com>
2026-07-10 03:43:04 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,11 @@ export default defineConfig({
                 about: resolve(__dirname, 'about.html'),
                 contact: resolve(__dirname, 'contact.html'),
                 license: resolve(__dirname, 'license.html'),
-                resume: resolve(__dirname, 'resume.html')
+                resume: resolve(__dirname, 'resume.html'),
+                musc: resolve(__dirname, 'proj-music.html'),
+                photo: resolve(__dirname, 'proj-photo.html'),
+                inter: resolve(__dirname, 'proj-interactive.html'),
+                arch: resolve(__dirname, 'proj-arch.html')
             }
         }
     }

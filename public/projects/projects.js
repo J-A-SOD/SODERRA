30c01af8752c6e46d60d2c4726839ef76fee9f9c
J.A.Soderstrom <jas_archi@outlook.com>
2026-07-14 14:48:52 -0700

    adding images

---
+++ public/projects/projects.js
@@ -0,0 +1,5 @@
+console.log("project js file has initially loaded");
+console.log("version 1.1.1");
+console.log("project: SODERRA - subpage");
+console.log("property of JOHANNES SODERSTROM");
+history.scrollRestoration = "manual";

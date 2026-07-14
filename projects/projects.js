85d668f4deb0e467f537483c93ee6a94fdd2f148
J.A.Soderstrom <jas_archi@outlook.com>
2026-07-14 14:50:03 -0700

    rename

--- projects/projects.js
+++
@@ -1,5 +0,0 @@
-console.log("project js file has initially loaded");
-console.log("version 1.1.1");
-console.log("project: SODERRA - subpage");
-console.log("property of JOHANNES SODERSTROM");
-history.scrollRestoration = "manual";
